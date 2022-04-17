d0f727917260a9683ccf300b7054b1a082dabb3c
EC2 Default User <ec2-user@ip-172-31-81-254.ec2.internal>
2022-04-17 19:32:21 +0000

    Commiting the modules

---
+++ modules/globalvars/output.tf
@@ -0,0 +1,12 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Aamir"
+    "App"   = "Web"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "project-week"
+}
